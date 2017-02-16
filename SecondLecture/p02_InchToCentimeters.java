93f7ba2cda1a0d9c6f9c0804c6ecd4067225f621
Evgeni Vitliemov <e.vittlyemov@gmail.com>
2017-02-16 19:21:17 +0200

    Update and rename InchToCentimeters.java to p02_InchToCentimeters.java

--- SecondLecture/p02_InchToCentimeters.java
+++ SecondLecture/p02_InchToCentimeters.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class InchToCentimeters {
+public class p02_InchToCentimeters {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
