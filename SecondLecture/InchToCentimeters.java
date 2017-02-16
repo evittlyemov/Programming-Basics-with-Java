31f99814e1e401ec475d9ba142e61b72497c9363
Evgeni Vitliemov <e.vittlyemov@gmail.com>
2017-02-16 19:11:40 +0200

    Create InchToCentimeters.java

---
+++ SecondLecture/InchToCentimeters.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InchToCentimeters {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Inches = ");
+
+        double inches = Double.parseDouble(scanner.nextLine());
+        double centimeters = inches * 2.54;
+
+        System.out.println("Centimeters = " + centimeters);
+    }
+}
