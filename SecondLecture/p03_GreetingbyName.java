ec497cd34f21f984f63302b793aa9cefb5ca2398
Evgeni Vitliemov <e.vittlyemov@gmail.com>
2017-02-16 19:22:26 +0200

    Create p03_GreetingbyName.java

---
+++ SecondLecture/p03_GreetingbyName.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p03_GreetingbyName {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+
+       // System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+
+    }
+}
