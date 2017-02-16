5c9d84a5b16dbc21658629b913338af207cd0bb9
Evgeni Vitliemov <e.vittlyemov@gmail.com>
2017-02-16 20:05:27 +0200

    Create p03_EvenOrOdd.java

---
+++ ThirdLecture/p03_EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class p03_EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if (num % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+
+    }
+}
