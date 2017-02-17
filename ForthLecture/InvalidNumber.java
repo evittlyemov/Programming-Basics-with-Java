a5d5167232f53c1dbd185096b866e9d4251324ee
Evgeni Vitliemov <e.vittlyemov@gmail.com>
2017-02-17 18:27:08 +0200

    Create InvalidNumber.java

---
+++ ForthLecture/InvalidNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InvalidNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        boolean isValid = (number >= 100 && number <= 200) || (number == 0);
+
+        if (!isValid) {
+            System.out.println("Invalid");
+        }
+    }
+}
