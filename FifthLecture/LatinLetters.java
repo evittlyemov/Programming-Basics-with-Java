df872e13151af08c08a89b861cce56fb3260a54a
Evgeni Vitliemov <e.vittlyemov@gmail.com>
2017-02-17 18:37:22 +0200

    Create LatinLetters.java

---
+++ FifthLecture/LatinLetters.java
@@ -0,0 +1,9 @@
+public class LatinLetters {
+    public static void main(String[] args) {
+
+        for (char letter = 'a'; letter <= 'z' ; letter++) {
+            System.out.print(" " + letter);
+
+        }
+    }
+}
