61f53695ed4752c15f88f79e6d9a3c2f46f161b1
Evgeni Vitliemov <e.vittlyemov@gmail.com>
2017-02-17 18:36:48 +0200

    Create NumbersEndingIn7.java

---
+++ FifthLecture/NumbersEndingIn7.java
@@ -0,0 +1,11 @@
+public class NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000 ; i++) {
+            if (i % 10 == 7) {
+                System.out.println(i);
+            }
+            
+        }
+    }
+}
